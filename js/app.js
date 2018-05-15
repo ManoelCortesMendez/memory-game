d96d34ecf5d3fc76eee013cece063e5ac22523d1
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-05-15 16:43:25 +0200

    Clean HTML, CSS, and JS

--- js/app.js
+++ js/app.js
@@ -1,243 +1,226 @@
-/* Use IIFE to avoid polluting global scope */
+// Use IIFE to avoid polluting global scope
 (function () {
+  // Define variables
+  const cardIcons = [
+    'art-icon', 'art-icon',
+    'cloud-icon', 'cloud-icon',
+    'die-icon', 'die-icon',
+    'fire-icon', 'fire-icon',
+    'paw-icon', 'paw-icon',
+    'plane-icon', 'plane-icon',
+    'sun-icon', 'sun-icon',
+    'world-icon', 'world-icon'
+  ]; // List each item twice to create matching pairs
+  let movesCount = 0;
+  let matchesCount = 0;
+  let stars = 3;
+  let firstCard, secondCard; // Save cards between clicks
+  let startTime, elapsedTime;
+  let intervalId; // Id interval to later be able to stop it
 
-    /* Get elements */
+  // Get elements
+  const gameboard = document.getElementById('gameboard');
+  const cardFronts = document.getElementsByClassName('card-front');
+  const movesCounter = document.getElementById('moves-counter');
+  const starsCounter = document.getElementById('stars-counter');
+  const timer = document.getElementById('timer');
+  const resetBtn = document.getElementById('reset-btn');
+  const victoryModal = document.getElementById('victory-modal');
+  const statsModal = document.getElementsByClassName('stat');
+  const resetBtnModal = document.getElementById('reset-btn-modal');
 
-    // Define list of all card icons -- each appears twice to form pairs
-    const cardIcons = ["plane-icon", "art-icon", "cloud-icon", "die-icon", "fire-icon", "paw-icon", "sun-icon", "world-icon", "plane-icon", "art-icon", "cloud-icon", "die-icon", "fire-icon", "paw-icon", "sun-icon", "world-icon"];
-    const board = document.getElementById('board');
-    const cardFronts = document.getElementsByClassName('card-front');
-    const movesCounter = document.getElementById('moves-counter');
-    let movesCount = 0;
-    const starsCounter = document.getElementById('stars-counter');
-    const resetButton = document.getElementById('reset-button');
-    let firstCard, secondCard; /* Save between clicks -- see playGame() */
-    const victoryModal = document.getElementById('victory-modal');
-    const resetBtnModal = document.getElementById('reset-btn-modal');
-    let matchesCount = 0;
-    const modalStats = document.querySelectorAll('#stats>.stat');
-    let stars = 3;
-    const timer = document.getElementById('timer');
-    let startTime, elapsedTime;
-    let intervalId;
 
-    /* Declare functions */
+  // Declare functions
+  // Fisher-Yates shuffle -- see stackoverflow.com/a/2450976
+  function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
-    // Fisher-Yates shuffle: http://stackoverflow.com/a/2450976
-    function shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
 
-        while (currentIndex !== 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        return array;
+  function revealCard(cardContainer) {
+    // Get card front and reveal it
+    cardContainer.firstElementChild.classList.add('reveal-front');
+    cardContainer.firstElementChild.classList.remove('hide-front');
+
+    // Get card back and hide it
+    cardContainer.lastElementChild.classList.add('hide-back');
+    cardContainer.lastElementChild.classList.remove('reveal-back');
+  }
+
+  function hideCard(cardContainer) {
+    // Get card front and hide it
+    cardContainer.firstElementChild.classList.add('hide-front');
+    cardContainer.firstElementChild.classList.remove('reveal-front');
+
+    // Remove mismatched card styling
+    cardContainer.firstElementChild.classList.remove('mismatched-card');
+
+    // Get card back and reveal it
+    cardContainer.lastElementChild.classList.add('reveal-back');
+    cardContainer.lastElementChild.classList.remove('hide-back');
+  }
+
+  function startTimer() {
+    elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+    timer.innerText = `${elapsedTime} s`; // Update time displayed
+  }
+
+  function incrementMovesCounter() {
+    movesCount += 1;
+
+    // Increment displayed move counter once per two card flips
+    if (movesCount % 2 === 0) {
+      movesCounter.innerText = (movesCount === 2) ? `${movesCount/2} move` : `${movesCount/2} moves`;
+    }
+  }
+
+  function adjustStarsCounter() {
+    /* Thresholds:
+        - 30 moves or less: 3 stars
+        - 31 to 35 moves: 2 stars
+        - 36 moves or more: 1 star */
+    stars = (movesCount <= 30) ? 3 : (movesCount <= 35 ? 2 : 1);
+
+    if (stars === 2) {
+      starsCounter.children[2].classList.add('star-outlined');
+    } else if (stars === 1) {
+      starsCounter.children[1].classList.add('star-outlined');
+    }
+  }
+
+  function compareCards(firstCard, secondCard) {
+    // Get card's icon class and assign it to local variable for convenicence
+    const firstIcon = firstCard.firstElementChild.classList[2];
+    const secondIcon = secondCard.firstElementChild.classList[2];
+
+    // Return true if cards match, false otherwise
+    return (firstIcon === secondIcon) ? true : false;
+  }
+
+  function styleMatchedCard(cardContainer) {
+    cardContainer.firstElementChild.classList.add('matched-card');
+  }
+
+  function styleMismatchedCard(cardContainer) {
+    cardContainer.firstElementChild.classList.add('mismatched-card');
+  }
+
+  function checkIfVictory(currentMatches) {
+    if (currentMatches === 8) {
+      // Stop timer
+      clearInterval(intervalId);
+
+      // Set modal stats: final time, moves, and stars
+      statsModal[0].innerText = `Time: ${elapsedTime}s`;
+      statsModal[1].innerText = `Moves: ${movesCount/2}`;
+      statsModal[2].innerText = `Stars: ${stars}`;
+
+      // Display modal
+      victoryModal.showModal();
+    }
+  }
+
+  function resetGame(cardIcons, cardFronts) {
+    // Shuffle icons
+    const cardIconsShuffled = shuffle(cardIcons);
+
+    // Assign shuffled icons to card fronts
+    for (const index in cardIcons) {
+      cardFronts[index].classList.add(cardIcons[index]);
+    }
+  }
+
+  // Main function -- control game flow
+  function playGame(clickEvent) {
+    // Check if movesCount is pair -- if not, mismatch impossible so skip
+    if (movesCount % 2 === 0) {
+      // Get mismatching pair -- if there is one
+      const mismatchedPair = gameboard.querySelectorAll('.mismatched-card');
+
+      // If mismatching pair found, flip it down and reset its styling
+      if (mismatchedPair.length === 2) {
+        // Remove mismatching pair styling
+        mismatchedPair[0].classList.remove('mismatched-card');
+        mismatchedPair[1].classList.remove('mismatched-card');
+
+        // Flip mismatching pair down
+        hideCard(mismatchedPair[0].parentElement);
+        hideCard(mismatchedPair[1].parentElement);
+      }
     }
 
-    function resetGame(cardIcons, cardFronts) {
-        // Shuffle icons
-        const cardIconsShuffled = shuffle(cardIcons);
+    // Trigger only when a face-down card (ie a card back) is clicked
+    if (clickEvent.target.classList.contains('card-back')) {
+      // Increment moves counter
+      incrementMovesCounter();
 
-        // Assign shuffled icons to card fronts
-        for (index in cardIcons) {
-            cardFronts[index].classList.add(cardIcons[index]);
-        }
-    }
+      // If first move, start timer
+      if (movesCount === 1) {
+        startTime = Date.now()
 
-    function revealCard(cardContainer) {
-        // Get card front and reveal it
-        cardContainer.firstElementChild.classList.add('reveal-front');
-        cardContainer.firstElementChild.classList.remove('hide-front');
+        // Call startTimer() every 1 second
+        intervalId = window.setInterval(startTimer, 1000);
+      }
 
-        // Get card back and hide it
-        cardContainer.lastElementChild.classList.add('hide-back');
-        cardContainer.lastElementChild.classList.remove('reveal-back');
-    }
+      // Adjust stars
+      adjustStarsCounter();
 
-    function hideCard(cardContainer) {
-        // Get card front and hide it
-        cardContainer.firstElementChild.classList.add('hide-front');
-        cardContainer.firstElementChild.classList.remove('reveal-front');
+      // Get clicked card container
+      const cardContainer = clickEvent.target.parentElement;
 
-        // Remove mismatched card styling
-        cardContainer.firstElementChild.classList.remove('mismatched-card');
+      // Reveal clicked card
+      revealCard(cardContainer);
 
-        // Get card back and reveal it
-        cardContainer.lastElementChild.classList.add('reveal-back');
-        cardContainer.lastElementChild.classList.remove('hide-back');
-    }
+      // Assess whether cards match -- if moves counter is even
+      if (movesCount % 2 === 1) {
+        firstCard = cardContainer;
+      } else {
+        secondCard = cardContainer;
 
-    function compareCards(firstCard, secondCard) {
-        const firstIcon = firstCard.firstElementChild.classList[2];
-        const secondIcon = secondCard.firstElementChild.classList[2];
+        // Apply green styling to matches, red to mismatches
+        if (compareCards(firstCard, secondCard)) {
+          // Increment matches count
+          matchesCount += 1;
 
-        // Return true if cards match, false otherwise
-        if (firstIcon === secondIcon) {
-            return true;
+          // Check for a victory
+          checkIfVictory(matchesCount);
+
+          // Style matches
+          styleMatchedCard(firstCard);
+          styleMatchedCard(secondCard);
         } else {
-            return false;
+          // Style mismatches
+          styleMismatchedCard(firstCard);
+          styleMismatchedCard(secondCard);
         }
+      }
     }
+  }
 
-    function styleMatchedCard(cardContainer) {
-        cardContainer.firstElementChild.classList.add('matched-card');
-    }
+  /* Add event listeners */
+  gameboard.addEventListener('click', function (event) {
+    // Launch game when gameboard is clicked
+    playGame(event);
+  });
 
-    function styleMismatchedCard(cardContainer) {
-        cardContainer.firstElementChild.classList.add('mismatched-card');
-    }
+  resetBtn.addEventListener('click', function () {
+    // Reload page to reset
+    document.location.reload();
+  });
 
-    function incrementMovesCounter() {
-        movesCount += 1;
+  resetBtnModal.addEventListener('click', function () {
+    // Reload page to reset
+    document.location.reload();
+  });
 
-        // Increment displayed move counter once per two card flips
-        if (movesCount % 2 === 0) {
-            switch (movesCount) {
-                case 2:
-                    movesCounter.innerText = `${movesCount/2} move`;
-                    break;
-                default:
-                    movesCounter.innerText = `${movesCount/2} moves`;
-            }
-        }
-    }
-
-    function adjustStarsCounter() {
-        // If <= 30 moves, 3 stars; > 30, 2; > 35, 1 star; > 40, 0
-        if (movesCount > 35) {
-            stars = 1;
-        } else if (movesCount > 30) {
-            stars = 2;
-        }
-
-        switch (stars) {
-            case 2:
-                starsCounter.children[2].classList.add('star-outlined');
-                break;
-            case 1:
-                starsCounter.children[1].classList.add('star-outlined');
-        }
-    }
-
-    function checkIfVictory(currentMatches) {
-        if (currentMatches === 8) {
-            // Stop timer
-            clearInterval(intervalId);
-
-            // Set modal stats
-            modalStats[0].innerText = `Time: ${elapsedTime}s`;
-            modalStats[1].innerText = `Moves: ${movesCount/2}`;
-            modalStats[2].innerText = `Stars: ${stars}`;
-
-            // Display modal
-            victoryModal.showModal();
-        }
-    }
-
-    function startTimer() {
-        elapsedTime = Math.floor((Date.now() - startTime) / 1000);
-        timer.innerText = `${elapsedTime} s`;
-    }
-
-    /* Main function --- control flow of game */
-    function playGame(clickEvent) {
-        // Start timer on first move
-        if (movesCount === 0) {
-            startTime = Date.now()
-
-            // Call startTimer() every 1 second
-            intervalId = window.setInterval(startTimer, 1000);
-        }
-
-        // Check if movesCount is pair --- if not, no mismatched pairs
-        if (movesCount % 2 === 0) {
-            // Get mismatched pair --- if there is one
-            const mismatchedPair = board.querySelectorAll('div.mismatched-card');
-
-            // If mismatch found, flip it down and reset its styling
-            if (mismatchedPair.length === 2) {
-                // Remove mismatch styling
-                mismatchedPair[0].classList.remove('mismatched-card');
-                mismatchedPair[1].classList.remove('mismatched-card');
-
-                // Hide mismatched pair
-                hideCard(mismatchedPair[0].parentElement);
-                hideCard(mismatchedPair[1].parentElement);
-            }
-        }
-
-        // Trigger only when a face-down card (ie a card back) is clicked
-        if (clickEvent.target.classList.contains('card-back')) {
-            // Increment moves counter
-            incrementMovesCounter();
-
-            // Adjust stars
-            adjustStarsCounter();
-
-            // Get clicked card container
-            const cardContainer = clickEvent.target.parentElement;
-
-            // Reveal clicked card
-            revealCard(cardContainer);
-
-            // Assess whether cards match -- if moves counter is even
-            if (movesCount % 2 === 1) {
-                firstCard = cardContainer;
-            } else {
-                secondCard = cardContainer;
-
-                // Apply appropriate styling for match or mismatch
-                if (compareCards(firstCard, secondCard)) {
-                    // Increment matches count
-                    matchesCount += 1;
-
-                    // Check for a victory
-                    checkIfVictory(matchesCount);
-
-                    // Style matches
-                    styleMatchedCard(firstCard);
-                    styleMatchedCard(secondCard);
-                } else {
-                    // Style mismatches
-                    styleMismatchedCard(firstCard);
-                    styleMismatchedCard(secondCard);
-                }
-            }
-        }
-    }
-
-    /* Add event listeners */
-
-    // Launch game when board is clicked
-    board.addEventListener('click', function (event) {
-        playGame(event);
-    });
-
-    resetButton.addEventListener('click', function () {
-        // Reload page to reset
-        document.location.reload();
-    });
-
-    resetBtnModal.addEventListener('click', function () {
-        // Reload page to reset
-        document.location.reload();
-    });
-
-    /* Call functions */
-
-    // Reset game immediately after page load --- ie initial set-up
-    resetGame(cardIcons, cardFronts);
+  /* Call functions */
+  resetGame(cardIcons, cardFronts); // Set up game on page load
 })();
-
-// TODO: commit regularly and push to remote
-
-// TODO: change star rating to go down to 1, not 0
-
-// TODO: ensure code conforms to all Udacity style guides
-
-// TODO: update README with game details and dependencies
-
-// TODO: clean comments one last time
